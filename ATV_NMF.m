5e28e9ea65258b16393b55b832dea24b7a65d4ce
tuntun1 <50696006+tuntun1@users.noreply.github.com>
2021-01-13 15:27:44 +0800

    Add files via upload

---
+++ ATV_NMF.m
@@ -0,0 +1,15 @@
+function [W,H,err]=ATV_GNMF(X,options,k,S,D)  
+maxiter =options.maxIter;
+[i,u]=size(X);
+r = k;                                 
+W=rand(i,r);                            
+H=rand(r,u);                                 
+alpha =options.alpha;
+for iter=1:maxiter
+   divHH=ATV_div(H);%%        
+   W=W.*((X*H')./(W*H*H'));  
+    W = max(W,eps);
+    H=H.*((W'*X+alpha*H*S+divHH)./(W'*W*H+alpha*H*D));
+    H = max(H,eps);   
+    err(iter)=norm(X-W*H,'fro')^2;
+end
